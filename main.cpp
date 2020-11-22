daf2adffa87ba5b6e84e6a363203dc9ed4fc5078
Pascal COMBES <pascom@orange.fr>
2020-11-22 15:27:50 +0100

    Added a light and a dark theme option.
    Used the light theme icons in the documentation.
    The light theme is used by default.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <QMenu>
 #include <QSystemTrayIcon>
 #include <QApplication>
+#include <QCommandLineParser>
 
 #include <QtDebug>
 
@@ -28,7 +29,11 @@
 
 int main(int argc, char *argv[])
 {
+    // Setup application:
     QApplication app(argc, argv);
+    QApplication::setApplicationName("ShutdownMonitor");
+    QApplication::setApplicationVersion("0.0.1");
+    QApplication::setOrganizationName("pascom");
 
     // Check that we are under X11:
     if (!QX11Info::isPlatformX11()) {
@@ -42,11 +47,28 @@ int main(int argc, char *argv[])
         return -2;
     }
 
+    // Setup command line arguments parser:
+    QCommandLineParser parser;
+    parser.addVersionOption();
+    parser.addHelpOption();
+    parser.setApplicationDescription("Enable and disable the monitors from a system tray icon");
+    parser.addOption(QCommandLineOption("theme", "The theme to use for the icons. It can be 'light' or 'dark'.", "theme", "light"));
+    parser.process(app);
+
+    // Check theme:
+    if (!QStringList({"light", "dark"}).contains(parser.value("theme"))) {
+        qWarning() << "Unsupported theme:" << parser.value("theme");
+        parser.showHelp(-3);
+        //return -3;
+    }
+
     // Load screen resources:
     QRRScreenResources* resources = QRRScreenResources::getCurrent(QX11Info::display());
 
     // Create the system tray menu:
     QMenu menu;
+    QIcon  enabledMonitorIcon(QString(":/icons/%1/enabled-monitor.png").arg(parser.value("theme")));
+    QIcon disabledMonitorIcon(QString(":/icons/%1/disabled-monitor.png").arg(parser.value("theme")));
     foreach (QRROutput* output, resources->outputs()) {
         if (output->connection != RR_Connected)
             continue;
@@ -54,14 +76,14 @@ int main(int argc, char *argv[])
 
         // Create an action for this connected output:
         QAction *action = menu.addAction(output->display());
-        action->setIcon(output->enabled() ? QIcon(":/icons/enabled-monitor.png") : QIcon(":/icons/disabled-monitor.png"));
-        QObject::connect(action, &QAction::triggered, [action, output] {
+        action->setIcon(output->enabled() ? enabledMonitorIcon : disabledMonitorIcon);
+        QObject::connect(action, &QAction::triggered, [action, output, &enabledMonitorIcon, &disabledMonitorIcon] {
             if (output->enabled())
                 output->disable();
             else
                 output->enable();
 
-            action->setIcon(output->enabled() ? QIcon(":/icons/enabled-monitor.png") : QIcon(":/icons/disabled-monitor.png"));
+            action->setIcon(output->enabled() ? enabledMonitorIcon : disabledMonitorIcon);
         });
     }
 
